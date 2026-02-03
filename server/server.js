20fba89de7100525114ca7e37fa77c86a9013f58
oArchito <138355786+oArchito@users.noreply.github.com>
2026-02-03 23:44:51 +0530

    authentication routes
    Added authentication routes to the server.

--- server/server.js
+++ server/server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
 const app = express();
+
+app.use(express.json());
 
 // connect database
 connectDB();
@@ -12,6 +15,9 @@ connectDB();
 app.get("/", (req, res) => {
   res.send("Online Exam Backend Running");
 });
+
+// auth routes
+app.use("/api/auth", authRoutes);
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Server running");
