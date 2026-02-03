37b24a9884ac0e55b4e31c1a59e488d70a36c7a3
oArchito <138355786+oArchito@users.noreply.github.com>
2026-02-03 23:54:45 +0530

    JWT expiration 1 to 3

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -56,7 +56,7 @@ const loginUser = async (req, res) => {
     const token = jwt.sign(
       { id: user._id, role: user.role },
       process.env.JWT_SECRET,
-      { expiresIn: "1d" }
+      { expiresIn: "3d" }
     );
 
     res.json({
