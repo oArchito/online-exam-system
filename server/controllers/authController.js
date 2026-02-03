d0c8e8d2242012726236cfbb762e4adb58f98828
oArchito <138355786+oArchito@users.noreply.github.com>
2026-02-03 23:41:54 +0530

    Implement user registration

---
+++ server/controllers/authController.js
@@ -0,0 +1,30 @@
+const User = require("../models/User");
+
+//regster
+const registerUser = async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const user = await User.create({
+      name,
+      email,
+      password,
+      role
+    });
+
+    res.status(201).json({
+      message: "User registered successfully"
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: "Server error"
+    });
+  }
+};
+
+module.exports = { registerUser };
