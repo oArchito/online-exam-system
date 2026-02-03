46430dd5cd510648095641f93820c815975106f2
oArchito <138355786+oArchito@users.noreply.github.com>
2026-02-03 23:36:43 +0530

    Connect to MongoDB before starting the server
    Added MongoDB connection setup before starting the server.

--- server/server.js
+++ server/server.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
+
+// connect the mongo connection
+connectDB();
+
+app.get("/", (req, res) => {
+  res.send("Online Exam Backend Running");
+});
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
-});
-app.get("/", (req, res) => {
-  res.send("Online Exam Backend Running");
 });
