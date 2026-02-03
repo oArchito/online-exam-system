9c3aa041a7c284225533bddb930459884b240a71
oArchito <138355786+oArchito@users.noreply.github.com>
2026-02-03 23:42:45 +0530

    Add authentication routes for user registration

---
+++ server/routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const { registerUser } = require("../controllers/authController");
+
+const router = express.Router();
+
+// register route
+router.post("/register", registerUser);
+
+module.exports = router;
